717cc4967f02c5b5378c6a6d79a967741228d828
Vasya67 <126453437+Vasya67@users.noreply.github.com>
2023-12-06 21:46:06 -1200

    FootballStadium.java

--- FootballStadium.java
+++ FootballStadium.java
@@ -1,24 +1,24 @@
-public class FootballStadium {// Static variables
+public class FootballStadium {
     private static String stadiumName;
     private static int capacity;
 
-    // Static block for initialization
+    
     static {
         stadiumName = "Example Stadium";
         capacity = 50000;
     }
 
-    // Static method 1
+    
     public static void displayStadiumInfo() {
         System.out.println("Stadium Name: " + stadiumName);
         System.out.println("Capacity: " + capacity);
     }
 
-    // Static method 2
+   
     public static void performActionWithStadium() {
-        // Calling static method 1
+        
         displayStadiumInfo();
-        // Perform additional actions
+       
         System.out.println("Performing action with the stadium...");
     }
 }
